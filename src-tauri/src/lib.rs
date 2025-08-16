fc68384088cbb455138a91cd18869977a3dd6d57
gregsadetsky <lepetitg@gmail.com>
2025-08-16 19:22:07 -0400

    Add missing Emitter trait import
    - Import tauri::Emitter to enable window.emit() method
    - Fixes compilation error for periodic update checking

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 use std::time::{Duration, Instant};
-use tauri::{Manager};
+use tauri::{Manager, Emitter};
 use tauri_plugin_cli::CliExt;
 use url::Url;
 use serde::Deserialize;
